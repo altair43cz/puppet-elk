5f05171d739c058b426365b3aba9ef0f98db32a4
Král <jkral@monetplus.cz>
2022-12-01 22:19:43 +0100

    clear out nginx

--- elk.pp
+++ elk.pp
@@ -1,7 +1,6 @@
 # https://forge.puppet.com/modules/puppet/elastic_stack
 include elastic_stack::repo
 package { 'default-jdk': ensure => 'installed' }
-include nginx
 package { 'elasticsearch': ensure => 'installed' }
 service { 'elasticsearch': ensure => 'running' }
 package { 'kibana': ensure => 'installed' }
