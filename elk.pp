84d50324e2c77460aab9d6dc4775f3540a3bbe59
Král <jkral@monetplus.cz>
2022-12-01 22:49:02 +0100

    logstash output.conf

--- elk.pp
+++ elk.pp
@@ -9,3 +9,21 @@ package { 'logstash': ensure => 'installed' }
 service { 'logstash': ensure => 'running' }
 file { '/etc/logstash/conf.d/02-beats-input.conf': 
          content => 'input { beats { port => 5044 } }' }
+
+file { '/etc/logstash/conf.d/30-elasticsearch-output.conf':
+         content => 'output {
+          if [@metadata][pipeline] {
+            elasticsearch {
+            hosts => ["localhost:9200"]
+            manage_template => false
+            index => "%{[@metadata][beat]}-%{[@metadata][version]}-%{+YYYY.MM.dd}"
+            pipeline => "%{[@metadata][pipeline]}"
+            }
+          } else {
+            elasticsearch {
+            hosts => ["localhost:9200"]
+            manage_template => false
+            index => "%{[@metadata][beat]}-%{[@metadata][version]}-%{+YYYY.MM.dd}"
+            }
+          }
+        }' }
