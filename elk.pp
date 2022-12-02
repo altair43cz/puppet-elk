60a4823de415dceb61c0973eec711c5f88687c58
Král <jkral@monetplus.cz>
2022-12-03 00:34:15 +0100

    try bd8365d114f

--- elk.pp
+++ elk.pp
@@ -10,8 +10,15 @@ service { 'logstash': ensure => 'running' }
 
 file { '/etc/logstash/conf.d/10-syslog-elasticsearch.conf':
          content => '#
-         input { file { path => "/var/log/apache/access.log" } }
-         filter { grok { match => { "message" => "%{COMBINEDAPACHELOG}" } }
-             date { match => [ "timestamp" , "dd/MMM/yyyy:HH:mm:ss Z" ] }
-             geoip { source => "clientip" } }
+         input { file { path => "/var/log/syslog" } }
+         filter { grok { match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} \[%{DATA}\] %{DATA} %{WORD:type} \"userText\":\"%{DATA:userText}\",\"prduction\":%{NUMBER:prduction:int},\"version\":%{NUMBER:version:int},\"bool\":%{DATA:bool},\{\"geoPoint\":\{\"location\":\"%{DATA:location}\",\"ip\":\"%{IP:ip}\",\"latitude\":%{BASE10NUM:latitude},\"longitude\":%{BASE10NUM:longitude},(\"optionalField\":%{BASE10NUM:optionalField},)?\}\}" }
+                add_field => [ "read_es", "%{@timestamp}" ] }
+            date { match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ] }
+            mutate { remove_field => [ "message" ]
+                remove_field => [ "syslog_timestamp" ]
+                convert => ["bool","boolean"]
+                rename => { "ip" => "[geoPoint][ip]" }
+                rename => { "latitude" => "[geoPoint][latitude]" }
+                rename => { "longitude" => "[geoPoint][longitude]" }
+                rename => { "location" => "[geoPoint][location]" } }
          output { elasticsearch { hosts => ["localhost:9200"] } }' }
