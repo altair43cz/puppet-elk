7c7dbc6d69b0ed51647117487e3e91eca8b3b930
Král <jkral@monetplus.cz>
2022-12-03 00:58:13 +0100

    try 63498970

--- elk.pp
+++ elk.pp
@@ -11,14 +11,5 @@ service { 'logstash': ensure => 'running' }
 file { '/etc/logstash/conf.d/10-syslog-elasticsearch.conf':
          content => '#
          input { file { path => "/var/log/syslog" } }
-         filter { grok { match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} \[%{DATA}\] %{DATA} %{WORD:type} \"userText\":\"%{DATA:userText}\",\"prduction\":%{NUMBER:prduction:int},\"version\":%{NUMBER:version:int},\"bool\":%{DATA:bool},\{\"geoPoint\":\{\"location\":\"%{DATA:location}\",\"ip\":\"%{IP:ip}\",\"latitude\":%{BASE10NUM:latitude},\"longitude\":%{BASE10NUM:longitude},(\"optionalField\":%{BASE10NUM:optionalField},)?\}\}" }
-                add_field => [ "read_es", "%{@timestamp}" ] }
-            date { match => [ "syslog_timestamp", "MMM  d HH:mm:ss", "MMM dd HH:mm:ss" ] }
-            mutate { remove_field => [ "message" ]
-                remove_field => [ "syslog_timestamp" ]
-                convert => ["bool","boolean"]
-                rename => { "ip" => "[geoPoint][ip]" }
-                rename => { "latitude" => "[geoPoint][latitude]" }
-                rename => { "longitude" => "[geoPoint][longitude]" }
-                rename => { "location" => "[geoPoint][location]" } }
+         filter { grok { match => { "message" => "%{SYSLOGTIMESTAMP:syslog_timestamp} %{SYSLOGHOST:syslog_hostname} %{DATA:syslog_program}(?:\[%{POSINT:syslog_pid}\])?: %{GREEDYDATA:syslog_message}" } } }
          output { elasticsearch { hosts => ["localhost:9200"] } }' }
